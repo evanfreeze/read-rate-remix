39becd2efb2ad5d8d5ca3d482bd1ff2be39dd8fd
Evan Freeze <evanfreeze@gmail.com>
2021-11-28 00:02:40 -0600

    round line cap on circle progress

--- app/components/CircleProgress.tsx
+++ app/components/CircleProgress.tsx
@@ -38,6 +38,7 @@ export default function CircleProgress({
                     style={{
                         strokeDasharray: circ,
                         strokeDashoffset: circ * position,
+                        strokeLinecap: "round",
                     }}
                 />
             </svg>
